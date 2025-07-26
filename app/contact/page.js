ea107dcc80e095a48044b830aedab91185b16caf
farazsheraz <farazahmed9094@gmail.com>
2025-07-26 18:33:28 +0500

    koi contatc pg

--- app/contact/page.js
+++ app/contact/page.js
@@ -47,7 +47,7 @@ export default function ContactPage({ imageUrl, title, text }) {
                         {/* Contact Information */}
                         <div className="p-5  border-[1px] border-amber-50 rounded-2xl backdrop-blur-lg bg-[linear-gradient(0deg,rgba(25,66,48,0.5),rgba(25,66,48,0.5)),linear-gradient(180deg,rgba(255,255,255,0.12)_0%,rgba(153,153,153,0.06)_100%)]">
                             {/* <h2 className="text-[#98C1A9] text-lg font-bold">Company Information</h2> */}
-                            <div className="flex justify-between items-center gap-5 my-5">
+                            <div className="flex justify-between items-center gap-5 mb-5">
                                 <div className="flex justify-start w-full gap-2 p-2 items-center border-[1px] border-amber-50 rounded-2xl">
                                     <div>
 
@@ -96,7 +96,7 @@ export default function ContactPage({ imageUrl, title, text }) {
                         {/* Office  Hours */}
                         <div className="p-5  border-[1px] border-amber-50 rounded-2xl backdrop-blur-lg bg-[linear-gradient(0deg,rgba(25,66,48,0.5),rgba(25,66,48,0.5)),linear-gradient(180deg,rgba(255,255,255,0.12)_0%,rgba(153,153,153,0.06)_100%)]">
                             <h2 className="text-[#98C1A9] text-lg font-bold">Office Hours</h2>
-                            <div className="flex justify-between items-center gap-5 my-5">
+                            <div className="flex justify-between items-center gap-5 mt-5">
                                 <div className="flex justify-start w-full gap-2 p-2 items-center border-[1px] border-amber-50 rounded-2xl ps-2.5">
                                     <div> <h3 className="font-bold">Mon - Thu</h3>
                                         <p>9:00 AM - 6:00 PM</p></div>
@@ -119,7 +119,7 @@ export default function ContactPage({ imageUrl, title, text }) {
                         <iframe
                             className="rounded-2xl border-0"
                             width="100%"
-                            height="441"
+                            height="370"
                             frameBorder="0"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.179816753767!2d67.04083572379122!3d24.857707420854073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f5125392865%3A0x94d6b804727306b0!2sCreatiVibe%20Labs!5e0!3m2!1sen!2s!4v1752566721319!5m2!1sen!2s"
                             allowFullScreen
